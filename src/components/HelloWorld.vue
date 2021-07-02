544879ae2ab187c2992b31da57cab5b12ee4d04e
HuyLN19 <almngoc@gmail.com>
2021-07-02 11:17:53 +0700

    [VueJS][MyCV]Built for Heroku

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,501 @@
+<template>
+  <div class="container bg-light mycv">
+        <div class="row">
+            <div class="col-xs-12 col-md-3 cat-title">
+            </div>
+            <div class="col-xs-12 col-md-9">
+                <p class="myname">Joseph Bear</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-md-3 cat-title">
+                <p>Personal info</p>
+            </div>
+            <div class="col-xs-12 col-md-9 border-bottom-primary u-info">
+                <div class="sub-content">
+                    <i class='bx bx-user'></i>
+                    <p>01,01,1980</p>
+                </div>
+                <div class="sub-content">
+                    <i class='bx bx-map'></i>
+                    <p> <span>1600 Parkway</span> , <span>95043 Cupertino</span> , <span>USA</span> </p>
+                </div>
+                <div class="sub-content">
+                    <i class='bx bx-envelope' ></i>
+                    <p>hello@kickresume.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-md-3 cat-title">
+                <p>Profile</p>
+            </div>
+            <div class="col-xs-12 col-md-9 border-bottom-primary">
+                <p>Results-oriented and motivated Financial Planning Analyst with 2+ years of 
+                    experience in analyzing examining financial data developing new financial forecsts
+                    , and improving the budgeting processes. The Employee of the Month Award winner with excellent
+                    analytical skills and an Accounting & Economics the top institution.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-md-3 cat-title">
+                <p>Work experience</p>
+            </div>
+            <div class="col-xs-12 col-md-9 border-bottom-primary">
+                <p class="sub-content-title">
+                    New Home Counselor <br>
+                    Carolina One
+                </p>
+                <div class="time-address">
+                    <i class='bx bx-calendar-alt' ></i>
+                    <span>06/2013 - present</span>
+                    <i class='bx bx-map'></i>
+                    <span>
+                        MOUNT PLEASANT
+                    </span>
+                </div>
+                <div>
+                    <ul class="content-para">
+                        <li>
+                            Collected and analyzed the company's financial data and results and worked on the
+                            <br> development of monthly financial forecasts.
+                        </li>
+                        <li>
+                            Collaborated with colleagues on the improvement of existing financial metrics to maximize 
+                            <br> efficiency - increased the efficiency by 24%.
+                        </li>
+                        <li>
+                            Managed and coordinated the forecasting processes and procedures, assisted in the creation 
+                            <br>of business models, and completed weekly reports.
+                        </li>
+                        <li>
+                            Significantly improved the budgeting processes, worked closely with financial advisors and 
+                            <br> other industry professionals, and performed other tasks as required.
+                        </li>
+                        <li>
+                            Awarded Employee of the Month for achieving exceptional results.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-md-3 cat-title">
+                <p>Education</p>
+            </div>
+            <div class="col-xs-12 col-md-9 border-bottom-primary">
+                <p class="sub-content-title">
+                    Language & International Business <br>
+                    Clemson University
+                </p>
+                <div class="time-address">
+                    <i class='bx bx-calendar-alt' ></i>
+                    <span>08/2006 - 05/2010</span>
+                    <i class='bx bx-map'></i>
+                    <span>UNITED STATES</span>
+                </div>
+                <div>
+                    <ul class="list-unstyled">
+                        <li>
+                            GPA: 3.98 (Top 15% of the Program)
+                        </li>
+                        <li>
+                            The 2014 Academic Excellence Award winner
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-md-3 cat-title">
+                <p>Languages</p>
+            </div>
+            <div class="col-xs-12 col-md-9 border-bottom-primary">
+                <div class="row languages-content mt-0">
+                    <div class="col-xs-12 col-md-4 d-flex my-2 p-0">
+                        <div class="pie-progress">
+                            <canvas id="english-progress"></canvas>
+                            <div class="percent">
+                                <span>100</span>
+                                <br>
+                                <span class="percent-icon">%</span>
+                            </div>
+                        </div>
+                        
+                        <span class="label-progress">English</span>
+                    </div>
+                    <div class="col-xs-12 col-md-4 d-flex my-2 p-0">
+                        <div class="pie-progress">
+                            <canvas id="french-progress"></canvas>
+                            <div class="percent">
+                                <span>95</span>
+                                <br>
+                                <span class="percent-icon">%</span>
+                            </div>
+                        </div>
+                        <span class="label-progress">French</span>
+                    </div>
+                    <div class="col-xs-12 col-md-4 d-flex my-2 p-0">
+                        <div class="pie-progress">
+                            <canvas id="spanish-progress"></canvas>
+                            <div class="percent">
+                                <span>80</span>
+                                <br>
+                                <span class="percent-icon">%</span>
+                            </div>
+                        </div>
+                        <span class="label-progress">Spanish</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-md-3 cat-title">
+                <p>Skills</p>
+            </div>
+            <div class="col-xs-12 col-md-9 border-bottom-primary mt-1">
+                <div class="cat-skill-title">
+                    <i class='bx bx-error-circle'></i>
+                    <p class="mb-0">SOFTWARE</p>
+                </div>
+                <div class="row mt-0 mb-5 u-skills">
+                    <div class="col-xs-12 col-md-4">
+                        <span>Microsoft Word</span>
+                        <br>
+                        <div class="d-flex flex-row">
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-4">
+                        <span>AtTask</span>
+                        <br>
+                        <div class="d-flex flex-row">
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-none-fill"></span>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-4">
+                        <span>Photoshop</span>
+                        <br>
+                        <div class="d-flex flex-row">
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-none-fill"></span>
+                            <span class="dot-rating-none-fill"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="cat-skill-title">
+                    <i class='bx bx-error-circle'></i>
+                    <p class="mb-0">DESIGN</p>
+                </div>
+                <div class="row mt-0 mb-4 u-skills">
+                    <div class="col-xs-12 col-md-4">
+                        <span>Drawing</span>
+                        <br>
+                        <div class="d-flex flex-row">
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-none-fill"></span>
+                            <span class="dot-rating-none-fill"></span>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-4">
+                        <span>Painting</span>
+                        <br>
+                        <div class="d-flex flex-row">
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-none-fill"></span>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-4">
+                        <span>Paintbrush</span>
+                        <br>
+                        <div class="d-flex flex-row">
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                            <span class="dot-rating-fill"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-md-3 cat-title">
+                <p>Strengths</p>
+            </div>
+            <div class="col-xs-12 col-md-9 border-bottom-primary">
+                <div class="row">
+                    <div class="col-xs-12 col-md-10 p-0">
+                        <span class="badge badge-primary">Polyglotism</span>
+                        <span class="badge badge-primary">Communication</span>
+                        <span class="badge badge-primary">Time Management</span>
+                        <span class="badge badge-primary">Leadership</span>
+                        <span class="badge badge-primary">Trading Background</span>
+                        <span class="badge badge-primary">Responsible</span>
+                        <span class="badge badge-primary">Problem-solving</span>
+                        <span class="badge badge-primary">Critical thinking</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-md-3 cat-title">
+                <p>Awards</p>
+            </div>
+            <div class="col-xs-12 col-md-9">
+                <p class="sub-content-title">
+                    The Employee of the Month <br>
+                    FR Group, Inc
+                </p>
+                <div class="time-address">
+                    <i class='bx bx-calendar-alt' ></i>
+                    <span>03/2015</span>
+                </div>
+                <div class="content-para">
+                    <p>Awarded for achieving extraordinary result, meeting all goals, and constantly executing
+                        <br>perfect work </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Fira+Sans:wght@200;300;400;500;600;700;800&display=swap');
+@import url('https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css');
+
+@media only screen and (max-width: 768px) {
+    /* For mobile phones: */
+    [class*="col-xs-12"] {
+      width: 100% !important;
+    }
+    .cat-title > p {
+        margin: 0;
+        text-align: center !important;
+    }
+    .pie-progress-box {
+        width: 80% !important;
+    }
+    ul {
+        padding: 15px !important;
+    }
+    .mycv {
+        padding: 44px 15px 100px 25px !important;
+    }
+    .u-skills > div {
+        margin-top: 2px !important;
+        margin-bottom: 10px !important;
+    }
+    .u-skills {
+        margin-bottom: 2em !important;
+    }
+    .cat-title > p {
+        margin-bottom: 10px !important;
+    }
+  }
+
+  @media only screen and (max-width: 1200px) {
+    .content-para > p > br {
+        display: none !important;
+    }
+    .content-para > li > br {
+        display: none !important;
+    }
+  }
+
+body {
+    background-color: black;
+    margin: 0;
+    font-size: 20px;
+    font-family: 'Fira Sans', sans-serif;
+    font-weight: 400;
+    color: #3d3d3d;
+}
+
+i {
+    font-size: 24px;
+    margin-right: 8px;
+}
+
+.mycv {
+    padding: 44px 0 100px 0;
+}
+
+.col-12, .col-md-9 {
+    padding: 0 !important;
+}
+
+.row {
+    margin: 15px 0;
+}
+
+.myname {
+    font-size: 46px;
+    font-weight: 700;
+    color: #9B876A;
+}
+
+.cat-title {
+    width: 19%;
+    font-size: 24px;
+    font-weight: 700;
+    color: #253a3a;
+    margin: 0 40px 0 0; 
+    padding: 0;
+}
+
+.cat-title > p {
+    margin: 0;
+    text-align: right;
+}
+
+.border-bottom-primary {
+    border-bottom: 2px solid #9B876A !important;
+}
+
+.sub-content-title {
+    color: black;
+    font-weight: 700;
+    font-size: 25px;
+    margin-bottom: 0 !important;
+    line-height: 27px;
+    padding-top: 6px;
+}
+
+.sub-content {
+    padding: 2px 0;
+    display: flex;
+    flex-direction: row;
+}
+
+.u-info > .sub-content > p {
+    position: relative;
+    bottom: 3px;
+}
+
+.time-address {
+    color: #9B876A;
+    font-size: 16px;
+    letter-spacing: 2px;
+    line-height: 20px;
+    vertical-align: middle;
+}
+
+.time-address > i {
+    margin-right:  0 !important;
+}
+
+.time-address > span {
+    position: relative;
+    bottom: 3px;
+}
+
+.pie-progress {
+    display: block;
+    width: 70px;
+    height: 70px;
+}
+
+.percent {
+    color: white;
+    font-size: 19px;
+    text-align: center;
+    position: relative;
+    bottom: 52px;
+}
+
+.percent-icon {
+    position: relative;
+    bottom: 13px;
+    font-size: 16px;
+}
+
+.languages-content {
+    margin-bottom: 32px; 
+    display: flex;
+    flex-direction: row;
+}
+
+.label-progress {
+    width: calc(100% - 70px);
+    position: relative;
+    top: 24px;
+}
+
+.cat-skill-title {
+    font-size: 17.5px;
+    font-weight: 500;
+    color: #9B876A;
+    letter-spacing: 2px;
+    display: flex;
+    flex-direction: row;
+}
+
+.cat-skill-title > i {
+    margin-right: 4px;
+}
+
+.dot-rating-fill {
+    width: 15px;
+    height: 15px;
+    background-color: #253a3a;
+    border-radius: 50%;
+    margin-right: 4px;
+}
+
+.dot-rating-none-fill {
+    width: 15px;
+    height: 15px;
+    background-color: #fff;
+    border: 2px solid #9B876A;
+    border-radius: 50%;
+    margin-right: 4px;
+    box-sizing: border-box;
+}
+
+.badge-primary {
+    color: #fff !important;
+    background-color: #9B876A !important;
+    font-size: 20px;
+    font-weight: 400 !important;
+    padding: 10px 17px;
+    border-radius: 0;
+    margin: 0 3px 7px 0;
+}
+
+ul {
+    padding: 0 !important;
+}
+
+li {
+    line-height: 27px;
+    margin-top: 2px;
+}
+</style>
